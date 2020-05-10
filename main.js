7873387b56886f3e8921699f63a501bb54cf887d
Larry Tseng <larry.tseng.1@gmail.com>
2020-05-10 18:15:28 -0400

    change scrool duration, add rocket

--- main.js
+++ main.js
@@ -12,5 +12,17 @@ typed.stop();
 
 $(document).ready(() => {
     setTimeout(() => typed.start(), 500);
-});
+    $("#main-rocket").hide();
 
+    $("#main-chevron").click((e) => {
+        let rocketSelector = $("#main-rocket");
+        rocketSelector.addClass("animate__animated animate__fadeIn");
+        rocketSelector.show();
+        $(".fa-chevron-down").hide();
+
+        setTimeout(() => {
+            $("#main-rocket").hide();
+            $(".fa-chevron-down").show();
+        }, 1500);
+    });
+});
